646607c7f85ac0c972f4728006f042bad8c3642c
Andre Boechat <boechat@msi-boechat>
2021-02-09 09:49:33 -0300

    Remove unnecessary code

--- personal/personal-fsharp.el
+++ personal/personal-fsharp.el
@@ -1,10 +1,7 @@
 (require 'prelude-programming)
 
 (prelude-require-packages '(fsharp-mode eglot-fsharp))
-(require 'fsharp-mode)
 
 (add-to-list 'auto-mode-alist '("\\.fs\\'" . fsharp-mode))
-
-(require 'eglot-fsharp)
 
 (provide 'prelude-fsharp)
