6ae7b035a0459b5f14f46b9afdb1200e5038c5f3
Andre Boechat <boechat@msi-boechat>
2021-03-27 19:52:45 -0300

    Make eglot work for Fsharp

--- personal/personal-fsharp.el
+++ personal/personal-fsharp.el
@@ -1,7 +1,12 @@
 (require 'prelude-programming)
 
 (prelude-require-packages '(fsharp-mode eglot-fsharp))
+(require 'fsharp-mode)
+;; WARNING: I couldn't make "eglot-fsharp" work out-of-box. I had to
+;; manually download "FsAutoComplete" and move to a directory
+;; structure similar to the one automatically created by "eglot-fsharp".
+(require 'eglot-fsharp)
 
 (add-to-list 'auto-mode-alist '("\\.fs\\'" . fsharp-mode))
 
-(provide 'prelude-fsharp)
+(provide 'personal-fsharp)
