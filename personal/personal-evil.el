c5a396ea6474ed4da8b9767b71edb79b175fed1a
Andre Boechat <boechat@msi-boechat>
2021-03-28 18:23:12 -0300

    Define more shortcuts for evil when using HS mode or Origami

--- personal/personal-evil.el
+++ personal/personal-evil.el
@@ -23,8 +23,25 @@
 (define-key evil-normal-state-map (kbd "-") 'evil-window-decrease-height)
 
 ;; Better folding shortcut.
-(require 'hideshow)
-(define-key evil-normal-state-map (kbd "zm") 'hs-hide-level)
-(define-key evil-normal-state-map (kbd "zM") 'hs-hide-all)
+(with-eval-after-load 'hideshow
+  (setq hs-hide-comments-when-hiding-all nil)
+  (evil-define-key 'normal hs-minor-mode-map
+    "zm" 'hs-hide-level
+    "zM" 'evil-close-folds))
+
+(with-eval-after-load 'origami
+  (defun personal-evil-fold-focus (buffer point)
+    (interactive (list (current-buffer) (point)))
+    (origami-show-only-node buffer point)
+    (origami-open-node-recursively buffer point))
+
+  (evil-define-key 'normal origami-mode-map
+    ;; Open and close folds.
+    "zA" 'origami-recursively-toggle-node
+    "zm" 'personal-evil-fold-focus
+    "zM" 'evil-close-folds
+    ;; Navigating through folds.
+    "zj" 'origami-next-fold
+    "zk" 'origami-previous-fold))
 
 (provide 'personal-evil)
