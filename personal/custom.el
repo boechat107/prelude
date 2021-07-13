dab3d98a6d28dba7ff6f700812552bb7edc3f8aa
Andre Ambrosio Boechat <boechat107@gmail.com>
2021-07-12 21:28:29 -0300

    Set agenda files; set new TODO keywords

--- personal/custom.el
+++ personal/custom.el
@@ -26,6 +26,9 @@
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
  '(objed-cursor-color "#cc6666")
+ '(org-agenda-files
+   '("~/Codes/Blogging/org-notes/notes.org" "~/Codes/Blogging/org-notes/tasks.org"))
+ '(org-todo-keywords '((sequence "TODO" "DOING" "|" "DONE")))
  '(package-selected-packages
    '(org-preview-html org-preview-html-mode scss-mode org-bullets tide origami cython-mode yaml-mode evil-vimish-fold vimish-fold neotree idle-highlight-mode doom-themes evil-collection json-mode eglot-fsharp fsharp-mode rust-mode key-chord prelude-evil helm-ag helm-descbinds helm-projectile helm exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
