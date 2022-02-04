268e76c6130c5b0315f067387c2ab9c77d0768c5
Andre Ambrosio Boechat <boechat107@gmail.com>
2022-02-04 12:01:30 -0300

    Set nodejs repl and default config for JS files

--- personal/personal-js.el
+++ personal/personal-js.el
@@ -2,5 +2,14 @@
 
 (prelude-require-package 'nodejs-repl)
 
-(with-eval-after-load 'js2-mode
-  (require 'nodejs-repl))
+(add-hook 'prelude-js-mode-hook
+          (lambda ()
+            (require 'nodejs-repl)
+            (subword-mode -1)
+            (hs-minor-mode +1)
+            (message "Loaded personal JS config")))
+(add-hook 'nodejs-repl-mode-hook
+          (lambda ()
+            ;; NOTE: The REPL mode doesn't support hs-minor-mode.
+            (smartparens-mode +1)
+            (message "Loaded personal JS REPL config")))
