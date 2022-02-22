f806ce6d45a4bb3165fbdebfa367fd4c6556b75d
Andre Ambrosio Boechat <andre.boechat@belvo.com>
2022-02-22 11:14:58 -0300

    Make text completion case sensitive for programming

--- personal/programming.el
+++ personal/programming.el
@@ -8,7 +8,9 @@
   (setq auto-save-default nil)
   (require 'super-save)
   (super-save-mode -1)
-
+  ;; This makes the text completion case sensitive.
+  ;; https://emacs.stackexchange.com/a/10838/31688
+  (setq company-dabbrev-downcase nil)
   (idle-highlight-mode +1)
   ;; Treat "_" as part of words.
   ;; https://evil.readthedocs.io/en/latest/faq.html#underscore-is-not-a-word-character
