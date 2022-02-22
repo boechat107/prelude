21c650d1896f650efc34ad60dee7fc72415761fe
Andre Ambrosio Boechat <andre.boechat@belvo.com>
2022-02-22 09:54:28 -0300

    Simplify Python module

--- personal/personal-python.el
+++ personal/personal-python.el
@@ -1,7 +1,6 @@
 (prelude-require-package 'poetry)
 
-(with-eval-after-load 'python-mode
-  (defun personal-python-mode-defaults ()
-    (hs-minor-mode +1))
+(defun personal-python-mode-defaults ()
+  (hs-minor-mode +1))
 
-  (add-hook 'python-mode-hook 'personal-python-mode-defaults))
+(add-hook 'python-mode-hook 'personal-python-mode-defaults)
