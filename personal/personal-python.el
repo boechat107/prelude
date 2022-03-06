d3e5389fd62d8e482a0b5a6f5ba7cf0194c8c2bd
Andre Ambrosio Boechat <andre.boechat@belvo.com>
2022-03-06 20:51:35 -0300

    Add better default config for Python flycheck

--- personal/personal-python.el
+++ personal/personal-python.el
@@ -1,6 +1,12 @@
-(prelude-require-package 'poetry)
+(prelude-require-packages '(poetry python-black))
+
+;; Stop asking about risky local variable settings.
+;; https://emacs.stackexchange.com/a/10989/31688
+(add-to-list 'safe-local-variable-values
+             '(flycheck-python-flake8-executable . "./venv/bin/python"))
 
 (defun personal-python-mode-defaults ()
+  (pyvenv-mode +1)
   (hs-minor-mode +1))
 
 (add-hook 'python-mode-hook 'personal-python-mode-defaults)
