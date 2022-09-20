d8c518e10bcfb6637519dd944a14e438a71d496e
Andre Ambrosio Boechat <boechat107@tutanota.com>
2022-09-20 12:53:35 -0300

    Make Git diffs (diff-hl) appear when using Emacs on terminal

--- personal/display.el
+++ personal/display.el
@@ -4,3 +4,10 @@
 (global-set-key [f8] 'neotree-toggle)
 
 (setq scroll-bar-width 5)
+
+;; Automatically enable Git diff at the sides when using Emacs on terminal.
+;; Ref.: https://github.com/dgutov/diff-hl/issues/155
+(add-hook 'diff-hl-mode-on-hook
+          (lambda ()
+            (unless (window-system)
+              (diff-hl-margin-mode))))
