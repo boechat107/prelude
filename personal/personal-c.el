4d72d8f512368603ebb439baa2a7f9f238714ba5
Andre Ambrosio Boechat <boechat107@gmail.com>
2022-11-20 19:38:38 -0300

    Require the package format-all and add comments about its usage

--- personal/personal-c.el
+++ personal/personal-c.el
@@ -1,5 +1,15 @@
 (require 'prelude-c)
 
+;; Formatting code with "format-all"
+;; https://github.com/lassik/emacs-format-all-the-code
+;;
+;; An example of a file ".clang-format"
+;; ---
+;; BasedOnStyle: LLVM
+;; IndentWidth: 4
+;; ---
+(prelude-require-packages '(format-all))
+
 (with-eval-after-load 'cc-mode
   (add-hook 'prelude-c-mode-common-hook
             (lambda ()
