a5bb57d97b3c3d72e0298edebcae07b8ae77b993
Andre Ambrosio Boechat <boechat107@gmail.com>
2023-11-05 19:34:29 -0300

    Move key definitions out of "with-eval-after-load"

--- personal/personal-evil.el
+++ personal/personal-evil.el
@@ -1,4 +1,4 @@
-(prelude-require-packages '(quelpa evil-collection origami))
+(prelude-require-packages '(evil-collection origami))
 
 ;; The latest version of "key-chord" produced constant failures. This is
 ;; reported in this issue:
@@ -37,33 +37,36 @@
 (require 'hideshow)
 (require 'origami)
 
-(with-eval-after-load 'hideshow
-  (defun personal-evil-hs-fold-focus (buffer point)
-    (interactive (list (current-buffer) (point)))
-    (evil-close-folds)
-    (evil-open-fold))
-
-  (setq hs-hide-comments-when-hiding-all nil)
-  (evil-define-key 'normal hs-minor-mode-map
+;; `evil-define-key' automatically delays the execution with
+;; `after-load-functions' if necessary.
+(evil-define-key 'normal hs-minor-mode-map
     "zm" 'hs-hide-level
     "zM" 'evil-close-folds
-    "zp" 'personal-evil-hs-fold-focus))
+    "zp" 'personal-evil-hs-fold-focus)
 
-(with-eval-after-load 'origami
-  ; TODO: define function and add as a hook.
-  (defun personal-evil-fold-focus (buffer point)
-    (interactive (list (current-buffer) (point)))
-    (origami-show-only-node buffer point)
-    (origami-open-node-recursively buffer point))
-
-  (evil-define-key 'normal origami-mode-map
+(evil-define-key 'normal origami-mode-map
     ;; Open and close folds.
     "zA" 'origami-recursively-toggle-node
     "zp" 'personal-evil-fold-focus
     "zM" 'evil-close-folds
     ;; Navigating through folds.
     "zj" 'origami-next-fold
-    "zk" 'origami-previous-fold))
+    "zk" 'origami-previous-fold)
+
+(with-eval-after-load 'hideshow
+  (defun personal-evil-hs-fold-focus (buffer point)
+    (interactive (list (current-buffer) (point)))
+    (evil-close-folds)
+    (evil-open-fold))
+
+  (setq hs-hide-comments-when-hiding-all nil))
+
+(with-eval-after-load 'origami
+  ; TODO: define function and add as a hook.
+  (defun personal-evil-fold-focus (buffer point)
+    (interactive (list (current-buffer) (point)))
+    (origami-show-only-node buffer point)
+    (origami-open-node-recursively buffer point)))
 
 (with-eval-after-load 'evil
   (defalias #'forward-evil-word #'forward-evil-symbol))
