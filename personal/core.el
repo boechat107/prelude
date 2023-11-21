3ced50bbd15fab7c76b3d328482fa7c6fb0f8471
Andre Ambrosio Boechat <boechat107@gmail.com>
2023-11-21 13:21:18 -0300

    Add option "ignore-space-at-eol" to magit diff

--- personal/core.el
+++ personal/core.el
@@ -15,3 +15,20 @@
 
 (add-hook 'jinx-mode-hook 'personal-jinx-mode-defaults)
 (global-jinx-mode +1)
+
+;; magit customization
+;; adding the diff option "--ignore-space-at-eol"
+;;
+;; I used the following doc page as a reference:
+;; https://magit.vc/manual/transient/Modifying-Existing-Transients.html
+;; `magit-diff' is the prefix we get when we press "d" on the status
+;; buffer. `magit-diff-refresh' is the one we get when we press "D".
+;; I needed to search for the existing options on magit's repository to find
+;; this info.
+(with-eval-after-load 'magit
+  (require 'magit-diff)
+  (require 'transient)
+
+  ;; "append" adds the new command after `-b'.
+  (transient-append-suffix 'magit-diff-refresh "-b"
+    '("-e" "Ignore space at EOL" ("" "--ignore-space-at-eol"))))
