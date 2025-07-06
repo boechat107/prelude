d829a2c56dd8dd082e55145f3155aa68dda11719
Andre Ambrosio Boechat <andre.boechat@tuta.io>
2025-07-06 18:29:05 -0300

    Fix installation of doom-xcode

--- personal/preload/display.el
+++ personal/preload/display.el
@@ -1,8 +1,5 @@
 ;;; -*- lexical-binding: t -*-
 (use-package doom-themes
-  :ensure t)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-
-;; Install package "gruvbox-theme" with "package-install".
-(setq prelude-theme 'doom-xcode)
+  :ensure t
+  :config
+  (setq prelude-theme 'doom-xcode))
